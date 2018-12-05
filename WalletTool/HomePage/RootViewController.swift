5411ca046ff87c9fb3743d2b329e717cc33e693d
caelanyang <caelan.yang@gmail.com>
2018-12-05 23:12:36 +0800

    add setting navigationBarItem

--- WalletTool/HomePage/RootViewController.swift
+++ WalletTool/HomePage/RootViewController.swift
@@ -16,6 +16,11 @@ class RootViewController: UIViewController {
         view.backgroundColor = UIColor.white
         title = "WalletTool"
         
+        //init navigationBaritem
+        let rightButton: UIBarButtonItem = UIBarButtonItem(title: "Setting", style: .plain, target: self, action: #selector(onSettingTap))
+        navigationItem.rightBarButtonItem = rightButton
+    
+        
         let label = UILabel.init(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         label.font = UIFont.systemFont(ofSize: 15)
         label.textColor = .black
@@ -37,14 +42,10 @@ class RootViewController: UIViewController {
         let walletVC = WalletCollectionViewController.init(collectionViewLayout: layout)
         navigationController?.pushViewController(walletVC, animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func onSettingTap() {
+        let settingVC = SettingViewController()
+        navigationController?.pushViewController(settingVC, animated: true)
     }
-    */
 
 }
--- WalletTool/SettingPage/SettingViewController.swift
+++ WalletTool/SettingPage/SettingViewController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+let SettingCellIdetifier: String = "SettingCellIdetifier"
+
+var dataArray:Array<String>?
+
+class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         title = "Setting"
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: SettingCellIdetifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        initView()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray?.count ?? 0
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingCellIdetifier, for: indexPath)
+        cell.textLabel?.text = dataArray?[indexPath.row]
+        return cell
+    }
+    func initView() {
+        dataArray = ["use touch ID"]
+    }
 }
